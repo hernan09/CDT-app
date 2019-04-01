bd5c08308c6cf9555e37bb2890c645bdfeba4320
hernan <hernag_09@hotmail.com>
2019-04-01 03:06:39 -0200

    reloj

--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -2,10 +2,30 @@ import React, { Component } from 'react';
 import './nav.css';
 
 class Nav extends Component {
+  constructor() {
+    super();
+    this.state = {
+      hour: 0
+    };
+    this.update = this.update.bind(this);
+  }
+
+  update(event) {
+    const date = new Date().toLocaleTimeString();
+    this.setState({
+      hour: date
+    });
+  }
+
+  componentWillMount() {
+    setInterval(this.update, 1000);
+    this.update();
+  }
+
   render() {
     return (
       <nav style={{ opacity: 0.7 }} className="navbar navbar-expand-lg navbar-dark bg-dark">
-        <a className="navbar-brand" href="#top"><span class="icon-home"></span> Home</a>
+        <a className="navbar-brand" href="#top"><span className="icon-home"></span> Home</a>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor03" aria-controls="navbarColor03" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon" />
         </button>
@@ -13,13 +33,9 @@ class Nav extends Component {
         <div className="collapse navbar-collapse" id="navbarColor03">
           <ul className="navbar-nav mr-auto">
             <li className="nav-item">
-              <a className="nav-link" href="#top">Features</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#top">Pricing</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#top">About</a>
+              <a className="nav-link" href="#top"><div className="clock-face">
+                <h1>{this.state.hour}</h1>
+              </div></a>
             </li>
           </ul>
         </div>
